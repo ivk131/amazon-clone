259a150d2f66cb21b2d43453dc9eecb4add52bb7
Vikash Kumar <vikashsmart1996@gmail.com>
2020-09-12 12:28:58 +0530

    Updates

---
+++ precache-manifest.ec86a858283be6138ff7392a9285a689.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36dfcfb4c9a090fa07f93af95deb9032",
+    "url": "/amazon-clone/index.html"
+  },
+  {
+    "revision": "45844c4686e7482195ab",
+    "url": "/amazon-clone/static/css/main.92937a0f.chunk.css"
+  },
+  {
+    "revision": "7ab4390b9234190f99b8",
+    "url": "/amazon-clone/static/js/2.a5c0ed72.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/amazon-clone/static/js/2.a5c0ed72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45844c4686e7482195ab",
+    "url": "/amazon-clone/static/js/main.0d270a5b.chunk.js"
+  },
+  {
+    "revision": "3101b3b2c4581d0f9087",
+    "url": "/amazon-clone/static/js/runtime-main.1cf4aa72.js"
+  }
+]);
